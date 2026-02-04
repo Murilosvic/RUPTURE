4b876b0f821d7864474b039abeac022bb2aa4e24
luiz selim <luizh.selim@gmail.com>
2026-02-04 19:56:06 -0300

    Finalização do Cadastro.js e correção no Cadastro.php

--- Controller/cadastro.php
+++ Controller/cadastro.php
@@ -1,27 +1,32 @@
 <?php
-header("content-Type: application/json");
+header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     http_response_code(405);
-    echo json_encode(["mensagem"=> "Metodo não permitido"]);
+    echo json_encode(["mensagem" => "Metodo nao permitido"]);
     exit;
-    $dados = json_decode(file_get_contents("php://input"), true);
-    $nome = $dados["nome"] ?? null;
-
-    if(!$name || !$email || !%$senha) {
-        http_response_code(400);
-        
-        //Aqui depois vai salvar no banco
-
-        http_response_code(201);
-        echo json_encode([
-            "mensagem" => "Usuario cadastrado com sucesso",
-            "nome" => $nome,
-            "email" = $email
-            
-        ])
-        
-    }
 }
+
+$dados = json_decode(file_get_contents("php://input"), true);
+
+$nome  = $dados["nome"]  ?? null;
+$email = $dados["email"] ?? null;
+$senha = $dados["senha"] ?? null;
+
+if (!$nome || !$email || !$senha) {
+    http_response_code(400);
+    echo json_encode(["mensagem" => "Dados obrigatórios não enviados"]);
+    exit;
+}
+
+// AQUI depois você salva no banco 👇
+// INSERT INTO Cliente ...
+
+http_response_code(201);
+echo json_encode([
+    "mensagem" => "Usuario cadastrado com sucesso",
+    "nome" => $nome,
+    "email" => $email
+]);
--- JS/cadastro.js
+++ JS/cadastro.js
@@ -13,5 +13,42 @@ try {
     const response = await()
 }
 const response = await fetch("")
+method: "POST" 
+
+headers: {
+    "Content-Type": "application/json"
+    //Infoma ao servidor que esta mandando dados em formato JSON
+    //Sem isso o backend pode não entender os dados
+
+    body: JSON.stringify({
+        nome.
+        email,
+        senha
+
+    //converte o objeto JS em uma string JSON
+    //é isso que o back end recebe no @requestBody
+
+    })
+
+    const data = await response.json();
+    //converte a resposta do servidor em um objeto JS
+    //converte de JSON para obsjeto JavaScript
+
+}
+
+if (response.ok) {
+    alert("Cadastro realizado com sucesso!");
+    console.log(data);
+
+}
+} else {
+    alert(data.mensagem || "Erro ao realizar cadastro");
+}  catch (error) {
+     console.error("Erro na requisição", error);
+}
+
+)
+
+}
 }
 
