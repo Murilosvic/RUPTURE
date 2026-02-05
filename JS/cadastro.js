1c075c3a0f2ae4f9c407daa9bf233f70254de6bb
Juliocsilvasantos <seu-email@exemplo.com>
2026-02-05 19:42:45 -0300

    Finalização e reparo no cadastro.js

--- JS/cadastro.js
+++ JS/cadastro.js
@@ -10,45 +10,41 @@ const email = document.getElementById("youremail").value;
 const senha = document.getElementById("yoursenha").value;
 
 try {
-    const response = await()
-}
-const response = await fetch("")
-method: "POST" 
+const response = await fetch("https://localhost/RUPTURE/Controller/cadastro.php", {
+
+method: "POST",
 
 headers: {
     "Content-Type": "application/json"
     //Infoma ao servidor que esta mandando dados em formato JSON
     //Sem isso o backend pode não entender os dados
-
+},
     body: JSON.stringify({
-        nome.
-        email,
-        senha
-
+    nome,
+    email,
+    senha
     //converte o objeto JS em uma string JSON
     //é isso que o back end recebe no @requestBody
 
     })
+});
 
-    const data = await response.json();
-    //converte a resposta do servidor em um objeto JS
-    //converte de JSON para obsjeto JavaScript
-
-}
+const data = await response.json();
+//converte a resposta do servidor em um objeto JS
+//converte de JSON para obsjeto JavaScript
 
 if (response.ok) {
     alert("Cadastro realizado com sucesso!");
     console.log(data);
 
-}
 } else {
     alert(data.mensagem || "Erro ao realizar cadastro");
+}
+
 }  catch (error) {
-     console.error("Erro na requisição", error);
-}
+    console.error("Erro na requisição", error);
+    alert("Erro de conexão com o servidor");
+    }
 
-)
-
-}
-}
+});
 
